bf5f3970a7710d3cafbb728e3a53d75bd0f28b4b
emirezer45 <emirezer4545@gmail.com>
2026-02-14 01:08:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,4 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+app.run_polling() 
