409c4a77ae3181d4e8aec92c6d381d7845ca05d4
emirezer45 <emirezer4545@gmail.com>
2026-02-14 05:52:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,10 +19,13 @@ from telegram.ext import (
 # ==============================
 
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("7729207035:AAHgaa3LzvETQ2k5Of_QPXdvZOR2-vUNh0o")
+
+if not TOKEN:
+    print("BOT_TOKEN bulunamadı!")
+else:
+    print("BOT_TOKEN bulundu!")
 
 if not TOKEN:
     raise ValueError("BOT_TOKEN environment variable is not set!")
