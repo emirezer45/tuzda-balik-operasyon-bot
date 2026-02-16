413c1806e775c4f03d29a3adb55b420cf88e18b1
emirezer45 <emirezer4545@gmail.com>
2026-02-16 14:58:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,8 @@ logging.basicConfig(level=logging.INFO)
 # =========================
 # AYARLAR
 # =========================
-TOKEN = "7729207035:AAEbnQtXwScP8AvscMFh67ZBfMrXJG1oZUU", "".strip()  # Railway Variables: BOT_TOKEN
+TOKEN = os.getenv("7729207035:AAEbnQtXwScP8AvscMFh67ZBfMrXJG1oZUU", "").strip()if not TOKEN:
+    raise ValueError("BOT_TOKEN boş! Railway Variables'a BOT_TOKEN ekle.")  # Railway Variables: BOT_TOKEN
 MUDUR_ID = 1753344846
 TZ = ZoneInfo("Europe/Istanbul")
 
